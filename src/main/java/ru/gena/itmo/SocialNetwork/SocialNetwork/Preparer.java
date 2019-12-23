a1b0cd4799198a4ec899dfca2453541abbdfdabf
Gena Ziganshin <gena.ziganshin@yandex.ru>
2019-12-23 19:27:10 +0300

    Preparer

--- src/main/java/ru/gena/itmo/SocialNetwork/SocialNetwork/Preparer.java
+++ src/main/java/ru/gena/itmo/SocialNetwork/SocialNetwork/Preparer.java
@@ -8,38 +8,38 @@ public class Preparer {
         //===========================================================================
         isTryingSuccess = instance
                 .executeQuery("DROP TABLE IF EXISTS MESSAGES;");
-        if (!isTryingSuccess) { return "problem with dropping users"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with dropping users"; }
         isTryingSuccess = instance
                 .executeQuery("DROP TABLE IF EXISTS USER_CONVERSATIONS;");
-        if (!isTryingSuccess) { return "problem with dropping users"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with dropping users"; }
         isTryingSuccess = instance
                 .executeQuery("DROP TABLE IF EXISTS USERS;");
-        if (!isTryingSuccess) { return "problem with dropping users"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with dropping users"; }
         isTryingSuccess = instance
                 .executeQuery("DROP TABLE IF EXISTS CONVERSATIONS;");
-        if (!isTryingSuccess) { return "problem with dropping users"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with dropping users"; }
         isTryingSuccess = instance
-                .executeQuery("CREATE TABLE IF NOT EXISTS USERS(" +
+                .executeQuery("CREATE TABLE IF NOT EXISTS USERS (" +
                 "ID INTEGER PRIMARY KEY, " +
                 "USERNAME VARCHAR(20), " +
                 "PASSWORD VARCHAR(20), " +
                 "FIRSTNAME VARCHAR(20), " +
                 "LASTNAME VARCHAR(20));");
-        if (!isTryingSuccess) { return "problem with users"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with users"; }
         isTryingSuccess = instance
-                .executeQuery("CREATE TABLE IF NOT EXISTS CONVERSATIONS(" +
+                .executeQuery("CREATE TABLE IF NOT EXISTS CONVERSATIONS (" +
                         "ID INTEGER PRIMARY KEY, " +
                         "NAME VARCHAR(20));");
-        if (!isTryingSuccess) { return "problem with conversations"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with conversations"; }
         isTryingSuccess = instance
-                .executeQuery("CREATE TABLE IF NOT EXISTS USER_CONVERSATIONS(" +
+                .executeQuery("CREATE TABLE IF NOT EXISTS USER_CONVERSATIONS (" +
                         "USER INTEGER, " +
                         "CONVERSATION INTEGER, " +
                         "CONSTRAINT FK_USER FOREIGN KEY (USER) REFERENCES USERS (ID), " +
                         "CONSTRAINT FK_CONV FOREIGN KEY (CONVERSATION) REFERENCES CONVERSATIONS (ID));");
-        if (!isTryingSuccess) { return "problem with user_conversation"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with user_conversation"; }
         isTryingSuccess = instance
-                .executeQuery("CREATE TABLE IF NOT EXISTS MESSAGES(" +
+                .executeQuery("CREATE TABLE IF NOT EXISTS MESSAGES (" +
                         "ID INTEGER PRIMARY KEY, " +
                         "CONVERSATION INTEGER, " +
                         "SENDER INTEGER, " +
@@ -47,102 +47,102 @@ public class Preparer {
                         "TEXT VARCHAR(20), " +
                         "CONSTRAINT FK_SENDER FOREIGN KEY (SENDER) REFERENCES USERS (ID), " +
                         "CONSTRAINT FK_CONVER FOREIGN KEY (CONVERSATION) REFERENCES CONVERSATIONS (ID));");
-        if (!isTryingSuccess) { return "problem with messages"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with messages"; }
         isTryingSuccess = instance
-                .executeQuery("INSERT INTO USERS VALUES(" +
+                .executeQuery("INSERT INTO USERS VALUES (" +
                         "100000, " +
                         "'admin', " +
                         "'la-la-la', " +
                         "'not', " +
                         "'not');");
-        if (!isTryingSuccess) { return "problem with insert values"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with insert values"; }
         isTryingSuccess = instance
-                .executeQuery("INSERT INTO CONVERSATIONS VALUES(" +
+                .executeQuery("INSERT INTO CONVERSATIONS VALUES (" +
                         "1, " +
                         "'testConversation');");
-        if (!isTryingSuccess) { return "problem with insert values1"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with insert values1"; }
         isTryingSuccess = instance
-                .executeQuery("INSERT INTO USER_CONVERSATIONS VALUES(" +
+                .executeQuery("INSERT INTO USER_CONVERSATIONS VALUES (" +
                         "100000, " +
                         "1);");
-        if (!isTryingSuccess) { return "problem with insert values2"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with insert values2"; }
         //===========================================================================
         isTryingSuccess = instance
                 .executeQuery("DROP TABLE IF EXISTS PATTERNSTREE;");
-        if (!isTryingSuccess) { return "problem with dropping users"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with dropping users"; }
         isTryingSuccess = instance
                 .executeQuery("DROP TABLE IF EXISTS PATTERNS;");
-        if (!isTryingSuccess) { return "problem with dropping users"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with dropping users"; }
         isTryingSuccess = instance
-                .executeQuery("CREATE TABLE IF NOT EXISTS PATTERNS(" +
+                .executeQuery("CREATE TABLE IF NOT EXISTS PATTERNS (" +
                         "ID INTEGER PRIMARY KEY, " +
                         "PATTERNSNAME VARCHAR(30), " +
                         "SITESWAP VARCHAR(30), " +
                         "DESCRIPTION VARCHAR(500));");
-        if (!isTryingSuccess) { return "problem with patterns";}
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with patterns";}
         isTryingSuccess = instance
-                .executeQuery("CREATE TABLE IF NOT EXISTS PATTERNSTREE(" +
+                .executeQuery("CREATE TABLE IF NOT EXISTS PATTERNSTREE (" +
                         "PATTERN INTEGER, " +
                         "DESCENDANTS INTEGER, " +
                         "CONSTRAINT FK_PATTERN FOREIGN KEY (PATTERN) REFERENCES PATTERNS (ID), " +
                         "CONSTRAINT FK_DESCENDANT FOREIGN KEY (DESCENDANTS) REFERENCES PATTERNS (ID));");
-        if (!isTryingSuccess) { return "problem with patternsTree"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with patternsTree"; }
         isTryingSuccess = instance
-                .executeQuery("INSERT INTO PATTERNS VALUES(" +
+                .executeQuery("INSERT INTO PATTERNS VALUES (" +
                         "0, " +
                         "'rootPattern', " +
                         "'', " +
                         "'');");
-        if (!isTryingSuccess) { return "problem with insert values0 into patterns"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with insert values0 into patterns"; }
         isTryingSuccess = instance
-                .executeQuery("INSERT INTO PATTERNS VALUES(" +
+                .executeQuery("INSERT INTO PATTERNS VALUES (" +
                         "1, " +
                         "'hand to hand throw', " +
                         "'once 3', " +
                         "'just do it');");
-        if (!isTryingSuccess) { return "problem with insert values1 into patterns"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with insert values1 into patterns"; }
         isTryingSuccess = instance
-                .executeQuery("INSERT INTO PATTERNS VALUES(" +
+                .executeQuery("INSERT INTO PATTERNS VALUES (" +
                         "2, " +
                         "'throw up', " +
                         "'once 4', " +
                         "'just do it');");
-        if (!isTryingSuccess) { return "problem with insert values2 into patterns"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with insert values2 into patterns"; }
         isTryingSuccess = instance
-                .executeQuery("INSERT INTO PATTERNS VALUES(" +
+                .executeQuery("INSERT INTO PATTERNS VALUES (" +
                         "3, " +
                         "'cascade', " +
                         "'3', " +
                         "'just do it');");
-        if (!isTryingSuccess) { return "problem with insert values3 into patterns"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with insert values3 into patterns"; }
         isTryingSuccess = instance
-                .executeQuery("INSERT INTO PATTERNS VALUES(" +
+                .executeQuery("INSERT INTO PATTERNS VALUES (" +
                         "4, " +
                         "'441', " +
                         "'441', " +
                         "'just do it');");
-        if (!isTryingSuccess) { return "problem with insert values4 into patterns"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with insert values4 into patterns"; }
 
         isTryingSuccess = instance
-                .executeQuery("INSERT INTO PATTERNSTREE VALUES(" +
+                .executeQuery("INSERT INTO PATTERNSTREE VALUES (" +
                         "0, " +
                         "1);");
-        if (!isTryingSuccess) { return "problem with insert values01 into patternsTree"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with insert values01 into patternsTree"; }
         isTryingSuccess = instance
-                .executeQuery("INSERT INTO PATTERNSTREE VALUES(" +
+                .executeQuery("INSERT INTO PATTERNSTREE VALUES (" +
                         "0, " +
                         "2);");
-        if (!isTryingSuccess) { return "problem with insert values02 into patternsTree"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with insert values02 into patternsTree"; }
         isTryingSuccess = instance
-                .executeQuery("INSERT INTO PATTERNSTREE VALUES(" +
+                .executeQuery("INSERT INTO PATTERNSTREE VALUES (" +
                         "1, " +
                         "3);");
-        if (!isTryingSuccess) { return "problem with insert values13 into patternsTree"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with insert values13 into patternsTree"; }
         isTryingSuccess = instance
-                .executeQuery("INSERT INTO PATTERNSTREE VALUES(" +
+                .executeQuery("INSERT INTO PATTERNSTREE VALUES (" +
                         "2, " +
                         "4);");
-        if (!isTryingSuccess) { return "problem with insert values24 into patternsTree"; }
+        if (!isTryingSuccess) { return "\n\n\n\nproblem with insert values24 into patternsTree"; }
         return null;
     }
 }
